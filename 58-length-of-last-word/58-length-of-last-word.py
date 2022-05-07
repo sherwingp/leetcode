db728b899dbef79ac8f9cc42f6791e3b984bfdfd
Sherwin Panganiban <67482908+sherwingp@users.noreply.github.com>
2022-05-07 21:11:07 +0100

    Time: 59 ms (11.54%), Space: 13.9 MB (80.81%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        window_start = len(s) - 1
+        window_end = None
+        while window_start >= 0:
+            if s[window_start].isalpha():
+                if not window_end:
+                    window_end = window_start
+                
+                if window_start == 0:
+                    return window_end - window_start + 1
+            else:
+                if window_end:
+                    return window_end - window_start
+            
+            window_start -= 1
+            
+        return 1
+            
